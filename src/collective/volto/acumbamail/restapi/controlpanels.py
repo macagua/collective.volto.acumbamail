726259dbe17f317b4e8558b2728ba0d6687321c8
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2026-04-12 19:10:55 +0200

    Fix the stop depending on IControlpanelLayer

--- src/collective/volto/acumbamail/restapi/controlpanels.py
+++ src/collective/volto/acumbamail/restapi/controlpanels.py
@@ -2,12 +2,15 @@
 
 from collective.volto.acumbamail.interfaces import ISettings
 from plone.restapi.controlpanels import RegistryConfigletPanel
-from plone.restapi.interfaces import IControlpanelLayer
+
+# from plone.restapi.interfaces import IControlpanelLayer
 from zope.component import adapter
 from zope.interface import Interface
+from zope.publisher.interfaces.browser import IBrowserRequest
 
 
-@adapter(Interface, IControlpanelLayer)
+# @adapter(Interface, IControlpanelLayer)
+@adapter(Interface, IBrowserRequest)
 class AcumbamailSettingsControlpanel(RegistryConfigletPanel):
     """REST API controlpanel adapter for Acumbamail settings."""
 
