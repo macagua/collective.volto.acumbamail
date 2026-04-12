1bf2c1c62d1a32488918017f329c47df2e516158
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2026-04-12 20:31:43 +0200

    Fixed the 'horse-with-no-namespace' problem when trying to pre-load collective.volto add-ons

--- src/collective/volto/__init__.py
+++ src/collective/volto/__init__.py
@@ -2,6 +2,14 @@
 try:
     __import__("pkg_resources").declare_namespace(__name__)
 except ImportError:
+    import sys
     from pkgutil import extend_path
 
-    __path__ = extend_path(__path__, __name__)
+    __path__[:] = extend_path(__path__, __name__)
+
+
+    def __getattr__(name):
+        fullname = f"{__name__}.{name}"
+        if fullname in sys.modules:
+            return sys.modules[fullname]
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
